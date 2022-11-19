dc5b84710284f11f8537d62fe55223e299075d77
MatthewDsouza <53507923+MatthewDsouza@users.noreply.github.com>
2022-11-19 15:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pytube import YouTube
 import os.path
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
 #     videoid= str(request.args.get('input'))
     videoid= "hello"
